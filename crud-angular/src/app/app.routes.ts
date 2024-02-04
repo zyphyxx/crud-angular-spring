947a8f42b452ee447d006189aa4efe0e0497d7ac
Edgar Jesus <mendesbr92@gmail.com>
2024-02-04 00:35:43 -0300

    feat: criando modulo do curso

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'courses'},
+
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
