c944019a535cf12547346f9613369e4b0d5aff7d
Edgar Jesus <mendesbr92@gmail.com>
2024-02-04 00:10:38 -0300

    feat: criando uma toolbars

--- crud-angular/src/app/app.config.ts
+++ crud-angular/src/app/app.config.ts
@@ -5,6 +5,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+
+
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
