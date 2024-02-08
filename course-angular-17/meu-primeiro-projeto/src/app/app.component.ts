e57ece6ad5bacf77ebf8a77c762ab722b9312007
Edgar Jesus <mendesbr92@gmail.com>
2024-02-07 22:31:05 -0300

    feat: components

--- course-angular-17/meu-primeiro-projeto/src/app/app.component.ts
+++ course-angular-17/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './componets/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NewComponent],
+
+  template: `
+
+    <h1>Curso de Angular</h1>
+    <app-new-component />
+
+`
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto';
